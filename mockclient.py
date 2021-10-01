f636d289aab1a07f816dc65a01046ce3fdb27e6d
Rew13 <thanadol_2000@windowslive.com>
2021-10-01 23:24:17 +0700

    Add timestamp field

--- mockclient.py
+++ mockclient.py
@@ -14,25 +14,43 @@ def send_new_data(client_id, token, secret, data, topic):
     client.publish(topic, payload=payload)
     client.disconnect()
 
-def pack_and_send(user_id, temp, oxy, hr, error_log):
+def set_error_log(msg):
+    global app, error_log, reset_times
+    error_log["text"] = msg
+    reset_times += 1
+    app.after(2000, reset_error_log)
+
+def reset_error_log():
+    global error_log, reset_times
+    if reset_times > 0:
+        reset_times -= 1
+    if reset_times == 0:
+        error_log["text"] = ""
+
+def pack_and_send(user_id, temp, oxy, hr, ts):
+    global DEFAULT_TIMESTAMP
+    if ts.get() == "":
+        ts.set(DEFAULT_TIMESTAMP)
     try:
-        print(user_id.get(), temp.get(), oxy.get(), hr.get())
+        #print(user_id.get(), temp.get(), oxy.get(), hr.get())
         data = {
             "temperature": temp.get(),
             'o2': oxy.get(),
             "heartrate": hr.get(),
-            "timestamp":'CURRENT_TIME'
-            }
-        error_log["text"] = "Success"
+            "timestamp": ts.get(),
+        }
         send_new_data(str(user_id.get()), "mqtt", "password", data, "homeIsolation")
+        set_error_log("Success")
     except tk.TclError:
-        error_log["text"] = "Error"
+        set_error_log("Error")
 
 PADDING_SIZE = 5
+reset_times = 0
+DEFAULT_TIMESTAMP = "CURRENT_TIME"
 
 if __name__ == "__main__":
     app = tk.Tk()
-    app.title("Housepital client (Mock)")
+    app.title("Housepital client (Mocked-up)")
 
     leftmaster = tk.Frame(app)
     leftmaster.pack(side=tk.LEFT, expand=True, padx=(PADDING_SIZE, PADDING_SIZE))
@@ -53,11 +71,15 @@ if __name__ == "__main__":
     hr_val = tk.DoubleVar(app, 89)
     tk.Entry(leftmaster, textvariable=hr_val).grid(row=3, column=1)
 
+    tk.Label(leftmaster, text="Timestamp:").grid(row=4, column=0, sticky=tk.W)
+    ts_val = tk.StringVar(app, DEFAULT_TIMESTAMP)
+    tk.Entry(leftmaster, textvariable=ts_val).grid(row=4, column=1)
+
     rightmaster = tk.Frame(app)
     rightmaster.pack(side=tk.RIGHT, expand=True, padx=(0, PADDING_SIZE))
     error_log = tk.Label(rightmaster)
-    tk.Button(rightmaster, text='Send data', command=lambda: pack_and_send(user_id_val, temp_val, oxy_val, hr_val, error_log)).pack(fill=tk.BOTH, expand=True)
-    error_log.pack(side=tk.BOTTOM)
+    tk.Button(rightmaster, text='Send data', command=lambda: pack_and_send(user_id_val, temp_val, oxy_val, hr_val, ts_val)).pack(fill=tk.BOTH, expand=True)
+    error_log.pack()
     
     app.resizable(width=False, height=False)
     app.mainloop()
